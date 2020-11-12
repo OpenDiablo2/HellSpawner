0fed76eb3f25c71d2460055c02011a80c672c32a
gravestench <dknuth0101@gmail.com>
2020-11-12 10:49:30 -0800

    adding hsinput package, implements input vectors

---
+++ hsinput/modifiers.go
@@ -0,0 +1,11 @@
+package hsinput
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+type Modifier int
+
+const (
+	ModAlt          = Modifier(ebiten.KeyAlt)
+	ModControl      = Modifier(ebiten.KeyControl)
+	ModShift        = Modifier(ebiten.KeyShift)
+)
