6e2a5dfa8a97cef7b8ad73491990f41bbbd8c7ee
Ian Ling <iling@datto.com>
2021-01-11 16:59:35 -0800

    Load last used project on startup

--- hsconfig/config.go
+++ hsconfig/config.go
@@ -12,9 +12,10 @@ import (
 )
 
 type Config struct {
-	RecentProjects   []string
-	AuxiliaryMpqPath string
-	ExternalListfile string
+	RecentProjects          []string
+	AuxiliaryMpqPath        string
+	ExternalListfile        string
+	OpenMostRecentOnStartup bool
 }
 
 func getConfigPath() string {
@@ -27,7 +28,8 @@ func getConfigPath() string {
 
 func generateDefaultConfig() *Config {
 	result := &Config{
-		RecentProjects: []string{},
+		RecentProjects:          []string{},
+		OpenMostRecentOnStartup: true,
 	}
 	result.Save()
 
@@ -71,28 +73,35 @@ func (c *Config) Save() error {
 }
 
 func (c *Config) AddToRecentProjects(filePath string) {
+	found := false
 	for idx := range c.RecentProjects {
 		if c.RecentProjects[idx] == filePath {
+			found = true
 			if idx != 0 {
 				old := c.RecentProjects[0]
 				c.RecentProjects[0] = filePath
 				c.RecentProjects[idx] = old
 			}
-			return
 		}
 	}
 
-	recent := []string{filePath}
-	for idx := range c.RecentProjects {
-		if idx == 5 {
-			break
+	if !found {
+		recent := []string{filePath}
+		for idx := range c.RecentProjects {
+			if idx == 5 {
+				break
+			}
+
+			recent = append(recent, c.RecentProjects[idx])
 		}
 
-		recent = append(recent, c.RecentProjects[idx])
+		c.RecentProjects = recent
 	}
 
-	c.RecentProjects = recent
-	c.Save()
+	err := c.Save()
+	if err != nil {
+		log.Fatalf("failed to save config: %s", err)
+	}
 }
 
 func (c *Config) GetAuxMPQs() []string {
