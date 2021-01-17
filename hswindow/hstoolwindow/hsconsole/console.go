a294b4a4e7fe6eea1244acc8019509def7648e82
Tim Sarbin <tim.sarbin@gmail.com>
2021-01-17 15:50:55 -0500

    Added console IO for debugging.

---
+++ hswindow/hstoolwindow/hsconsole/console.go
@@ -0,0 +1,45 @@
+package hsconsole
+
+import (
+	"github.com/OpenDiablo2/HellSpawner/hswindow/hstoolwindow"
+	g "github.com/ianling/giu"
+	"github.com/ianling/imgui-go"
+)
+
+type Console struct {
+	*hstoolwindow.ToolWindow
+	outputText string
+	fontFixed  imgui.Font
+}
+
+func (c *Console) Write(p []byte) (n int, err error) {
+	c.outputText = string(p) + c.outputText
+
+	return len(p), nil
+}
+
+func Create(fontFixed imgui.Font) *Console {
+	result := &Console{
+		fontFixed:  fontFixed,
+		ToolWindow: hstoolwindow.New("Console"),
+	}
+
+	return result
+}
+
+func (c *Console) Build() {
+	c.IsOpen(&c.Visible).
+		Pos(10, 500).
+		Size(600, 200).
+		Layout(g.Layout{
+			g.Custom(func() {
+				g.PushFont(c.fontFixed)
+			}),
+			g.InputTextMultiline("", &c.outputText).
+				Size(-1, -1).
+				Flags(g.InputTextFlagsReadOnly | g.InputTextFlagsNoUndoRedo),
+			g.Custom(func() {
+				g.PopFont()
+			}),
+		})
+}
