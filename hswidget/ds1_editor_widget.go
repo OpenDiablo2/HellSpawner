33b171c9827f60585f34cd77ef54e69bd9d83560
M. Sz <mszeptuch@protonmail.com>
2021-02-16 13:02:09 +0100

    ds1 editor: removing floor index in multi-floor ds1's doesn't crash app anymore

--- hswidget/ds1_editor_widget.go
+++ hswidget/ds1_editor_widget.go
@@ -487,6 +487,8 @@ func (p *DS1Widget) makeTileLayout(state *DS1ViewerState, t *d2ds1.TileRecord) g
 						p.deleteButtonTexture,
 						func() {
 							p.deleteFloorRecord()
+							p.ds1.NumberOfFloors--
+							p.recreateLayerStreamTypes()
 						},
 					),
 				),
@@ -1025,5 +1027,48 @@ func (p *DS1Widget) deleteFloorRecord() {
 	}
 
 	p.ds1.Tiles[state.tileY][state.tileX].Floors = newFloors
-	p.ds1.NumberOfFloors--
+
+}
+
+// Warning: this is 1:1 copy from
+// github.com/OpenDiablo2/OpenDiablo2/d2common/d2fileformats/d2ds1.(*DS1).setupLayerStreamType()
+// but this method is unexported for now, so...
+func (p *DS1Widget) recreateLayerStreamTypes() {
+	var layerStream []d2enum.LayerStreamType
+
+	// nolint:gomnd // this is constant version
+	// see in OpenDiablo2
+	if p.ds1.Version < 4 {
+		layerStream = []d2enum.LayerStreamType{
+			d2enum.LayerStreamWall1,
+			d2enum.LayerStreamFloor1,
+			d2enum.LayerStreamOrientation1,
+			d2enum.LayerStreamSubstitute,
+			d2enum.LayerStreamShadow,
+		}
+	} else {
+		// nolint:gomnd // constant (each wall layer has d2enum.LayerStreamWall and Orientation)
+		layerStream = make([]d2enum.LayerStreamType,
+			(p.ds1.NumberOfWalls*2)+p.ds1.NumberOfFloors+p.ds1.NumberOfShadowLayers+p.ds1.NumberOfSubstitutionLayers)
+
+		layerIdx := 0
+		for i := 0; i < int(p.ds1.NumberOfWalls); i++ {
+			layerStream[layerIdx] = d2enum.LayerStreamType(int(d2enum.LayerStreamWall1) + i)
+			layerStream[layerIdx+1] = d2enum.LayerStreamType(int(d2enum.LayerStreamOrientation1) + i)
+			layerIdx += 2
+		}
+		for i := 0; i < int(p.ds1.NumberOfFloors); i++ {
+			layerStream[layerIdx] = d2enum.LayerStreamType(int(d2enum.LayerStreamFloor1) + i)
+			layerIdx++
+		}
+		if p.ds1.NumberOfShadowLayers > 0 {
+			layerStream[layerIdx] = d2enum.LayerStreamShadow
+			layerIdx++
+		}
+		if p.ds1.NumberOfSubstitutionLayers > 0 {
+			layerStream[layerIdx] = d2enum.LayerStreamSubstitute
+		}
+	}
+
+	p.ds1.LayerStreamTypes = layerStream
 }
