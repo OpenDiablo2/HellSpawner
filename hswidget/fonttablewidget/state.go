73146f83f90c3f6e68c370144fc2286bfb2262f7
M. Sz <mszeptuch@protonmail.com>
2021-02-18 17:55:15 +0100

    font table editor: init of state

---
+++ hswidget/fonttablewidget/state.go
@@ -0,0 +1,52 @@
+package fonttablewidget
+
+import (
+	"fmt"
+
+	"github.com/ianling/giu"
+)
+
+type fontTableWidgetMode int
+
+const (
+	fontTableWidgetViewer fontTableWidgetMode = iota
+)
+
+type widgetState struct {
+	mode fontTableWidgetMode
+}
+
+// Dispose cleans state
+func (s *widgetState) Dispose() {
+	// noop
+}
+
+func (p *FontTableWidget) getStateID() string {
+	return fmt.Sprintf("COFWidget_%s", p.id)
+}
+
+func (p *FontTableWidget) getState() *widgetState {
+	var state *widgetState
+
+	s := giu.Context.GetState(p.getStateID())
+
+	if s != nil {
+		state = s.(*widgetState)
+	} else {
+		p.initState()
+		state = p.getState()
+	}
+
+	return state
+
+}
+
+func (p *FontTableWidget) initState() {
+	p.setState(&widgetState{
+		mode: fontTableWidgetViewer,
+	})
+}
+
+func (p *FontTableWidget) setState(s giu.Disposable) {
+	giu.Context.SetState(p.getStateID(), s)
+}
