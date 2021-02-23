da0a1359027e5069d50b80df3b11e487f0d4c3cd
M. Sz <mszeptuch@protonmail.com>
2021-02-23 20:46:15 +0100

    font table editor: rename stdMod -> childSizeModifier

--- hscommon/hsutil/imagebuttonhack.go
+++ hscommon/hsutil/imagebuttonhack.go
@@ -6,14 +6,14 @@ import (
 
 // it looks like childWidget's size must be a bit greater then widget iside of it
 const (
-	stdMod = 8
+	childSizeModifier = 8
 )
 
 // MakeImageButton is a hack for giu.ImageButton that creates image button
 // as a giu.child
 func MakeImageButton(id string, w, h int, t *giu.Texture, fn func()) giu.Layout {
 	return giu.Layout{
-		giu.Child(id+"child").Border(false).Size(float32(w+stdMod), float32(h+stdMod)).Layout(giu.Layout{
+		giu.Child(id+"child").Border(false).Size(float32(w+childSizeModifier), float32(h+childSizeModifier)).Layout(giu.Layout{
 			giu.ImageButton(t).Size(float32(w), float32(h)).OnClick(fn),
 		}).Flags(giu.WindowFlagsNoDecoration),
 	}
