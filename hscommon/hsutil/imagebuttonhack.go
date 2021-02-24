0c63ad12f2e5bb490dd01b1a364cc824e1065da3
M. Sz <mszeptuch@protonmail.com>
2021-02-24 08:15:25 +0100

    hsutil: imagebuttonhack refactor

--- hscommon/hsutil/imagebuttonhack.go
+++ hscommon/hsutil/imagebuttonhack.go
@@ -1,20 +1,33 @@
 package hsutil
 
 import (
-	"github.com/ianling/giu"
-)
+	"fmt"
 
-// it looks like childWidget's size must be a bit greater then widget iside of it
-const (
-	childSizeModifier = 8
+	"github.com/ianling/giu"
 )
 
 // MakeImageButton is a hack for giu.ImageButton that creates image button
 // as a giu.child
 func MakeImageButton(id string, w, h int, t *giu.Texture, fn func()) giu.Layout {
-	return giu.Layout{
-		giu.Child(id+"child").Border(false).Size(float32(w+childSizeModifier), float32(h+childSizeModifier)).Layout(giu.Layout{
-			giu.ImageButton(t).Size(float32(w), float32(h)).OnClick(fn),
-		}).Flags(giu.WindowFlagsNoDecoration),
+	const (
+		childIdSuffix = "child"
+		padding       = 8 // pixels
+	)
+
+	// the image button
+	btnW, btnH := float32(w), float32(h)
+	button := giu.Layout{
+		giu.ImageButton(t).Size(btnW, btnH).OnClick(fn),
 	}
+
+	// the container; needs to be padded to be larger than the button
+	childW, childH := btnW+padding, btnH+padding
+	childID := fmt.Sprintf("%s%s", id, childIdSuffix)
+	con := giu.Child(childID).
+		Border(false).
+		Size(float32(childW), float32(childH)).
+		Layout(button).
+		Flags(giu.WindowFlagsNoDecoration)
+
+	return giu.Layout{con}
 }
