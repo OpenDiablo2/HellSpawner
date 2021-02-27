20f4db306e0ed5ad8cda8d32f1b4815f44751fac
M. Sz <mszeptuch@protonmail.com>
2021-02-27 16:05:20 +0100

    anim data viewer: lintfix

--- hswidget/animdatawidget/widget.go
+++ hswidget/animdatawidget/widget.go
@@ -6,6 +6,10 @@ import (
 	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2fileformats/d2animdata"
 )
 
+const (
+	listW, listH = 200, 400
+)
+
 type widget struct {
 	id string
 	d2 *d2animdata.AnimationData
@@ -33,14 +37,15 @@ func (p *widget) Build() {
 
 func (p *widget) buildAnimationsList() {
 	state := p.getState()
+
 	list := make([]*giu.SelectableWidget, p.d2.GetRecordsCount())
-	for n, index := range state.mapKeys {
-		list[n] = giu.Selectable(index)
+	for idx, name := range state.mapKeys {
+		list[idx] = giu.Selectable(name)
 	}
 
 	giu.Layout{
 		giu.Child("##"+p.id+"keyList").Border(false).
-			Size(200, 200).
+			Size(listW, listH).
 			Layout(giu.Layout{
 				giu.Custom(func() {
 					for _, i := range list {
