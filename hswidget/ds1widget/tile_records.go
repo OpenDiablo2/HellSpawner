4dec42243c410a57fa5cd81e74c0d58f0cf176ac
M. Sz <mszeptuch@protonmail.com>
2021-03-03 11:05:40 +0100

    ds1 editor: removed add/deleteShadow methods (tiles must hav 1 shadow layer, so we can simply edit it)

--- hswidget/ds1widget/tile_records.go
+++ hswidget/ds1widget/tile_records.go
@@ -3,8 +3,6 @@ package ds1widget
 import (
 	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2enum"
 	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2fileformats/d2ds1"
-
-	"github.com/OpenDiablo2/HellSpawner/hswidget"
 )
 
 func (p *widget) createFloorShadowRecord() d2ds1.FloorShadowRecord {
@@ -119,56 +117,4 @@ func (p *widget) deleteWall() {
 
 	p.ds1.NumberOfWalls--
 	p.recreateLayerStreamTypes()
-}
-
-func (p *widget) addShadow() {
-	state := p.getState()
-
-	state.addFloorShadowState.cb = func() {
-		newShadow := p.createFloorShadowRecord()
-
-		for y := range p.ds1.Tiles {
-			for x := range p.ds1.Tiles[y] {
-				p.ds1.Tiles[y][x].Shadows = make([]d2ds1.FloorShadowRecord, 1)
-				p.ds1.Tiles[y][x].Shadows[0] = newShadow
-			}
-		}
-	}
-
-	p.ds1.NumberOfShadowLayers++
-	p.recreateLayerStreamTypes()
-
-	state.mode = ds1EditorModeAddFloorShadow
-}
-
-func (p *widget) deleteShadow() {
-	state := p.getState()
-
-	yesCB := func() {
-		for y := range p.ds1.Tiles {
-			for x := range p.ds1.Tiles[y] {
-				p.ds1.Tiles[y][x].Shadows = nil
-			}
-		}
-
-		p.ds1.NumberOfShadowLayers--
-		p.recreateLayerStreamTypes()
-
-		state.mode = ds1EditorModeViewer
-	}
-
-	state.confirmDialog = hswidget.NewPopUpConfirmDialog(
-		"##"+p.id+"removeShadowConfirm",
-		"Warning",
-		"non-shadow files aren't supported.\n"+
-			"If you'll delete shadow, and will not create\n"+
-			"a new one, the file will be destroyed and\n"+
-			"You will be unable to open it again.\n"+
-			"Continue?",
-		yesCB,
-		func() {
-			state.mode = ds1EditorModeViewer
-		},
-	)
-	state.mode = ds1EditorModeConfirm
 }
