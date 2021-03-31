459d612b25a9ac5f511f25571e692b77945ec19d
gucio321 <gucio321@protonmail.com>
2021-03-31 20:10:04 +0200

    cof widget: move stat management methods to state.go

--- hswidget/cofwidget/widget.go
+++ hswidget/cofwidget/widget.go
@@ -9,7 +9,6 @@ import (
 	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2enum"
 	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2fileformats/d2cof"
 
-	"github.com/OpenDiablo2/HellSpawner/hsassets"
 	"github.com/OpenDiablo2/HellSpawner/hscommon"
 	"github.com/OpenDiablo2/HellSpawner/hscommon/hsenum"
 	"github.com/OpenDiablo2/HellSpawner/hswidget"
@@ -70,64 +69,6 @@ func (p *widget) Build() {
 			state.confirmDialog,
 		}.Build()
 	}
-}
-
-func (p *widget) getStateID() string {
-	return fmt.Sprintf("widget_%s", p.id)
-}
-
-func (p *widget) getState() *widgetState {
-	var state *widgetState
-
-	s := giu.Context.GetState(p.getStateID())
-
-	if s != nil {
-		state = s.(*widgetState)
-	} else {
-		p.initState()
-		state = p.getState()
-	}
-
-	return state
-}
-
-func (p *widget) setState(s giu.Disposable) {
-	giu.Context.SetState(p.getStateID(), s)
-}
-
-func (p *widget) initState() {
-	state := &widgetState{
-		mode: modeViewer,
-		viewerState: &viewerState{
-			confirmDialog: &hswidget.PopUpConfirmDialog{},
-		},
-		newLayerFields: &newLayerFields{
-			selectable: true,
-			drawEffect: int32(d2enum.DrawEffectNone),
-		},
-	}
-
-	if len(p.cof.CofLayers) > 0 {
-		state.viewerState.layer = &p.cof.CofLayers[0]
-	}
-
-	p.textureLoader.CreateTextureFromFile(hsassets.UpArrowIcon, func(texture *giu.Texture) {
-		state.textures.up = texture
-	})
-
-	p.textureLoader.CreateTextureFromFile(hsassets.DownArrowIcon, func(texture *giu.Texture) {
-		state.textures.down = texture
-	})
-
-	p.textureLoader.CreateTextureFromFile(hsassets.LeftArrowIcon, func(texture *giu.Texture) {
-		state.textures.left = texture
-	})
-
-	p.textureLoader.CreateTextureFromFile(hsassets.RightArrowIcon, func(texture *giu.Texture) {
-		state.textures.right = texture
-	})
-
-	p.setState(state)
 }
 
 func (p *widget) makeViewerLayout() giu.Layout {
