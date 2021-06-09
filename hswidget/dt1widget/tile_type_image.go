b4709cca28e660a9ad644fde54aeb147c719718f
gucio321 <gucio321@protonmail.com>
2021-06-09 17:43:00 +0200

    dt1widget: cut a long method

--- hswidget/dt1widget/tile_type_image.go
+++ hswidget/dt1widget/tile_type_image.go
@@ -3,95 +3,31 @@ package dt1widget
 import (
 	"github.com/ianling/giu"
 
-	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2enum"
-
 	"github.com/OpenDiablo2/HellSpawner/hswidget/dt1widget/tiletypeimage"
+
+	"github.com/OpenDiablo2/OpenDiablo2/d2common/d2enum"
 )
 
 func drawTileTypeImage(t d2enum.TileType) giu.Widget {
-	tileImageFile := getTileTypeImage(t)
+	return giu.Custom(func() {
+		canvas := giu.GetCanvas()
+		pos := giu.GetCursorScreenPos()
+		b := tiletypeimage.TileTypeImage(canvas, pos)
+		lookup := map[d2enum.TileType]func(){
+			d2enum.TileFloor:                      func() { b.Floor() },
+			d2enum.TileLeftWall:                   func() { b.Floor().WestWall(true) },
+			d2enum.TileRightWall:                  func() { b.Floor().NorthWall(true) },
+			d2enum.TileRightPartOfNorthCornerWall: func() { b.Floor().WestWall(false).NorthWall(true) },
+			d2enum.TileLeftPartOfNorthCornerWall:  func() { b.Floor().WestWall(true).NorthWall(false) },
+			d2enum.TileLeftEndWall:                func() { b.Floor().EastWall() },
+			d2enum.TileRightEndWall:               func() { b.Floor().SoathWall() },
+			d2enum.TileSouthCornerWall:            func() { b.Floor().Corner() },
+			d2enum.TileLeftWallWithDoor:           func() { b.Floor().WestDoor() },
+			d2enum.TileRightWallWithDoor:          func() { b.Floor().NorthDoor() },
+		}
 
-	switch t {
-	case d2enum.TileFloor:
-		return giu.Custom(func() {
-			canvas := giu.GetCanvas()
-			pos := giu.GetCursorScreenPos()
-			b := tiletypeimage.TileTypeImage(canvas, pos)
-			b.Floor()
-		})
-	case d2enum.TileLeftWall:
-		return giu.Custom(func() {
-			canvas := giu.GetCanvas()
-			pos := giu.GetCursorScreenPos()
-			b := tiletypeimage.TileTypeImage(canvas, pos)
-			b.Floor().WestWall(true)
-		})
-	case d2enum.TileRightWall:
-		return giu.Custom(func() {
-			canvas := giu.GetCanvas()
-			pos := giu.GetCursorScreenPos()
-			b := tiletypeimage.TileTypeImage(canvas, pos)
-			b.Floor().NorthWall(true)
-		})
-	case d2enum.TileRightPartOfNorthCornerWall:
-		return giu.Custom(func() {
-			canvas := giu.GetCanvas()
-			pos := giu.GetCursorScreenPos()
-			b := tiletypeimage.TileTypeImage(canvas, pos)
-			b.Floor().WestWall(false).NorthWall(true)
-		})
-	case d2enum.TileLeftPartOfNorthCornerWall:
-		return giu.Custom(func() {
-			canvas := giu.GetCanvas()
-			pos := giu.GetCursorScreenPos()
-			b := tiletypeimage.TileTypeImage(canvas, pos)
-			b.Floor().WestWall(true).NorthWall(false)
-		})
-	case d2enum.TileLeftEndWall:
-		return giu.Custom(func() {
-			canvas := giu.GetCanvas()
-			pos := giu.GetCursorScreenPos()
-			b := tiletypeimage.TileTypeImage(canvas, pos)
-			b.Floor().EastWall()
-		})
-	case d2enum.TileRightEndWall:
-		return giu.Custom(func() {
-			canvas := giu.GetCanvas()
-			pos := giu.GetCursorScreenPos()
-			b := tiletypeimage.TileTypeImage(canvas, pos)
-			b.Floor().SoathWall()
-		})
-	case d2enum.TileSouthCornerWall:
-		return giu.Custom(func() {
-			canvas := giu.GetCanvas()
-			pos := giu.GetCursorScreenPos()
-			b := tiletypeimage.TileTypeImage(canvas, pos)
-			b.Floor().Corner()
-		})
-	case d2enum.TileLeftWallWithDoor:
-		return giu.Custom(func() {
-			canvas := giu.GetCanvas()
-			pos := giu.GetCursorScreenPos()
-			b := tiletypeimage.TileTypeImage(canvas, pos)
-			b.Floor().WestDoor()
-		})
-	case d2enum.TileRightWallWithDoor:
-		return giu.Custom(func() {
-			canvas := giu.GetCanvas()
-			pos := giu.GetCursorScreenPos()
-			b := tiletypeimage.TileTypeImage(canvas, pos)
-			b.Floor().NorthDoor()
-		})
-	default:
-		return giu.ImageWithFile("./hsassets/images/" + tileImageFile)
-	}
-}
-
-func getTileTypeImage(t d2enum.TileType) string {
-	switch t {
-	case d2enum.TileSouthCornerWall:
-		return "corner_lower_east.png"
-	default:
-		return ""
-	}
+		if creator, ok := lookup[t]; ok {
+			creator()
+		}
+	})
 }
